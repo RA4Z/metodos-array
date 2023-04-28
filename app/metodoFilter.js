c0c457c1665b902fef1b06f26320d6787265b950
RA4Z <rraz639@gmail.com>
2023-04-28 13:26:24 -0300

    Alterando livros que aparecem na tela de acordo com filtro escolhido

--- app/metodoFilter.js
+++ app/metodoFilter.js
@@ -5,5 +5,5 @@ function FiltrarLivros() {
     const elementoBtn = document.getElementById(this.id)
     const categoria = elementoBtn.value
     let livrosFiltrados = livros.filter(livro => livro.categoria == categoria)
-    console.table(livrosFiltrados)
+    exibirOsLivrosNaTela(livrosFiltrados)
 }
